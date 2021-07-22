ad40ac227448d716be5d180a4c5055330c0f2264
Jolanta Ijannidi <jolanta.ijannidi@gmail.com>
2021-07-22 16:30:02 +0300

    Decision Tree regressor model

--- src/main/scala/com/github/jolay_07/stockMarket/stockAnalysis.scala
+++ src/main/scala/com/github/jolay_07/stockMarket/stockAnalysis.scala
@@ -1,6 +1,10 @@
 package com.github.jolay_07.stockMarket
 
+import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.classification.{DecisionTreeClassifier, RandomForestClassifier}
+import org.apache.spark.ml.evaluation.RegressionEvaluator
+import org.apache.spark.ml.feature.VectorIndexer
+import org.apache.spark.ml.regression.{DecisionTreeRegressionModel, DecisionTreeRegressor, LinearRegression}
 import org.apache.spark.sql.functions.{avg, col, desc, expr, stddev}
 
 object stockAnalysis extends App{
@@ -72,19 +76,52 @@ object stockAnalysis extends App{
   val endDf = ndf.where("prevOpen != 0.0") // null isn't needed
   endDf.show(10, false)
 
+  val featureIndexer = new VectorIndexer()
+    .setInputCol("features")
+    .setOutputCol("indexedFeatures")
+    .setMaxCategories(4)
+    .fit(endDf)
 
-  val fittedRF = supervised.fit(endDf)
-  val preparedDF = fittedRF.transform(endDf)
-  preparedDF.show(false)
-  preparedDF.sample(0.1).show(false)
+  // Split the data into training and test sets (30% held out for testing).
+  val Array(trainingData, testData) = endDf.randomSplit(Array(0.7, 0.3))
 
-  val Array(train, test) = preparedDF.randomSplit(Array(0.75, 0.25))
-  val randomForest = new RandomForestClassifier()
+  // Train a DecisionTree model.
+  val dt = new DecisionTreeRegressor()
     .setLabelCol("label")
-    .setFeaturesCol("features")
-    .setNumTrees(10) //this is so called hyperparameter for this particular algorithm
+    .setFeaturesCol("indexedFeatures")
+  // Chain indexer and tree in a Pipeline.
+  val pipeline = new Pipeline()
+    .setStages(Array(featureIndexer, dt))
 
-  val fittedModel = randomForest.fit(train) //create a model
-  val testDF = fittedModel.transform(test)  //use this model to make predictions and save them in a DataFrame
-  testDF.show(30,false)
+  // Train model. This also runs the indexer.
+  val model = pipeline.fit(trainingData)
+
+  // Make predictions.
+  val predictions = model.transform(testData)
+  predictions.show(10, false)
+
+  // Select (prediction, true label) and compute test error.
+  val evaluator = new RegressionEvaluator()
+    .setLabelCol("label")
+    .setPredictionCol("prediction")
+    .setMetricName("rmse")
+  val rmse = evaluator.evaluate(predictions)
+  println(s"Root Mean Squared Error (RMSE) on test data = $rmse")
+
+//  val treeModel = model.stages(1).asInstanceOf[DecisionTreeRegressionModel]
+//  println(s"Learned regression tree model:\n ${treeModel.toDebugString}")
+
+//  val linReg = new LinearRegression()
+//
+//  val Array(train,test) = endDf.randomSplit(Array(0.75,0.25))
+//
+//  val lrModel = linReg.fit(train)
+//
+//  val intercept = lrModel.intercept
+//  val coefficients = lrModel.coefficients
+//  val x1 = coefficients(0)
+//  val x2 = coefficients(1) //of course we would have to know how many x columns we have as features
+//
+//  println(s"Intercept: $intercept and coefficient for x1 is $x1 and for x2 is $x2")
+//  //simple linear regression is unlikely to yield anything on financial data which foll
 }
